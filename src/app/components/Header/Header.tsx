5a208c82c0a0a8b3c1efcaf0195dc00e5d75eca0
Rowgie D. Tulipas <rowgietulipas@gmail.com>
2025-12-02 15:46:43 +0800

    Implementing Hero Icons, using Hamburger icon for smaller view

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 import Logo from "../Logo/Logo";
+import { Bars3BottomLeftIcon } from "@heroicons/react/16/solid";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 
 export default function Header() {
     return (
         <header className="flex px-3 py-3 ss:px-5 ss:py-5 justify-between items-center bg-transparent">
             <Logo />
-            <span className="sm:hidden">+</span>
+            <Bars3Icon className="h-5 w-5 sm:hidden"/>
             <nav className="hidden sm:flex lg:min-w-full">
                 <ul className="list-none flex flex-row justify-around gap-5 bg-red-300">
                     <li>
