7cbc85cdc249f09a67503fa735b7af69d114e958
Rowgie D. Tulipas <rowgietulipas@gmail.com>
2025-12-18 13:31:46 +0800

    Pool Slide and Beach Slide - Mobile Design

--- src/app/components/mainSwiper/slides/BeachSlide.tsx
+++ src/app/components/mainSwiper/slides/BeachSlide.tsx
@@ -5,21 +5,60 @@ function BeachSlide() {
         <section className="h-screen w-screen pt-15 bg-slate-900">
             <div className="absolute inset-0 opacity-60">
                 <Image
-                    src="/cable.jpg"
+                    src="/cove.jpg"
                     alt="Private Beach"
                     fill
                     className="w-full h-full object-cover"
                 />
             </div>
-            <div className="absolute inset-0 bg-gradient-to-t from-slate-900 via-slate-900/40 to-transparent"></div>
+            <div className="absolute inset-0 bg-linear-to-t from-slate-900 via-slate-900/40 to-transparent"></div>
 
-            <div className="relative z-10 container mx-auto px-6">
-                <div className="bg-white/10 backdrop-blur-md border border-white/20 p-8 rounded-2xl max-w-5xl mx-auto">
-                    <div className="flex flex-col md:flex-row gap-12 text-white">
-                        <div className="flex-1">
-                            <span className="text-teal-300 font-bold tracking-widest text-sm block mb-2">
-                                LEVEL 05 - SEA LEVEL
-                            </span>
+            <div className="h-full flex justify-center items-center">
+                <div className="relative z-10 container mx-auto px-6">
+                    <div className="bg-white/10 backdrop-blur-md border border-white/20 p-8 rounded-2xl max-w-5xl mx-auto">
+                        <div className="flex flex-col md:flex-row gap-5 text-white">
+                            <div className="flex flex-col">
+                                <span className="text-[.8rem] text-teal-300 font-bold tracking-widest text-sm block mb-2">
+                                    LEVEL 05 - Beach &amp; Cove
+                                </span>
+                                <h2 className="text-xl font-semibold font-serif mb-3">
+                                    Solterra Beach & Cove
+                                </h2>
+                                <p className="text-[.8rem] text-slate-200">
+                                    The final stop of the Funicular. Walk the
+                                    elegant wooden pier to reach pristine white
+                                    sands.
+                                </p>
+                            </div>
+
+                            <div className="flex-1 space-y-6">
+                                <div>
+                                    <h3 className="text-xl font-bold text-teal-200 mb-1">
+                                        Neptune&apos;s Beach Club
+                                    </h3>
+                                    <p className="text-sm text-slate-300">
+                                        Barefoot luxury dining under a massive
+                                        thatched roof.
+                                    </p>
+                                </div>
+                                <div>
+                                    <h3 className="text-xl font-bold text-teal-200 mb-1">
+                                        The Private Cove
+                                    </h3>
+                                    <p className="text-sm text-slate-300">
+                                        A hidden beach accessible only via a
+                                        torch-lit path through the rocks.
+                                    </p>
+                                </div>
+                                <div>
+                                    <h3 className="text-xl font-bold text-teal-200 mb-1">
+                                        Water Sports Hut
+                                    </h3>
+                                    <p className="text-sm text-slate-300">
+                                        Jetskis, paddleboards, and scuba diving.
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/app/components/mainSwiper/slides/PoolSlide.tsx
+++ src/app/components/mainSwiper/slides/PoolSlide.tsx
@@ -1,6 +1,7 @@
 import { Coffee, Droplets, Sun } from "lucide-react";
 import Image from "next/image";
 import { useSwiperSlide } from "swiper/react";
+import PoolCard from "../../PoolCard/PoolCard";
 
 function PoolSlide() {
     const { isActive } = useSwiperSlide();
@@ -17,86 +18,26 @@ function PoolSlide() {
                 </div>
                 <div className="h-6/7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
                     {/* Lagoon Pool and Bar */}
-                    <div className="relative inset-0">
-                        <div className="absolute inset-0">
-                            <Image
-                                src="/pool-bar.jpg"
-                                alt="Lagoon Pool & Sunken Bar"
-                                fill
-                                className="w-full h-full object-cover opacity-90 rounded-2xl"
-                            />
-                            <div
-                                className={`absolute inset-0 bg-linear-to-t from-slate-900/80 via-slate-900/30 to-transparent rounded-2xl
-                            `}
-                            />
-                        </div>
-                        <div className="relative z-10 h-full flex flex-col justify-end px-3 text-white">
-                            <div className="flex flex-row items-center gap-2 mb-2">
-                                <Droplets size={25} className="text-teal-500" />
-                                <span className="font-semibold">
-                                    Lagoon Pool & Sunken Bar
-                                </span>
-                            </div>
-                            <p className="text-left text-[.7rem] font-light mb-3">
-                                A massive free-form pool with a swim-up bar
-                                hidden under a cascading waterfall.
-                            </p>
-                        </div>
-                    </div>
+                    <PoolCard
+                        src="/pool-bar-1.jpg"
+                        icon={<Droplets size={25} className="text-teal-500" />}
+                        title="Lagoon Pool & Sunken Bar"
+                        description="A massive free-form pool with a swim-up bar hidden under a cascading waterfall."
+                    />
                     {/* Thermal Pool */}
-                    <div className="relative inset-0">
-                        <div className="absolute inset-0">
-                            <Image
-                                src="/pool-bar.jpg"
-                                alt="Thermal Pool"
-                                fill
-                                className="w-full h-full object-cover opacity-90 rounded-2xl"
-                            />
-                            <div
-                                className={`absolute inset-0 bg-linear-to-t from-slate-900/80 via-slate-900/30 to-transparent rounded-2xl
-                            `}
-                            />
-                        </div>
-                        <div className="relative z-10 h-full flex flex-col justify-end px-3 text-white">
-                            <div className="flex flex-row items-center gap-2 mb-2">
-                                <Sun size={25} className="text-teal-500" />
-                                <span className="font-semibold">
-                                    Thermal Pool
-                                </span>
-                            </div>
-                            <p className="text-left text-[.7rem] font-light mb-3">
-                                A circular hot pool built on the terrace edge
-                                for therapeutic soaking with a view.
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="relative inset-0">
-                        <div className="absolute inset-0">
-                            <Image
-                                src="/pool-bar.jpg"
-                                alt="Private Cabanas"
-                                fill
-                                className="w-full h-full object-cover opacity-90 rounded-2xl"
-                            />
-                            <div
-                                className={`absolute inset-0 bg-linear-to-t from-slate-900/80 via-slate-900/30 to-transparent rounded-2xl
-                            `}
-                            />
-                        </div>
-                        <div className="relative z-10 h-full flex flex-col justify-end px-3 text-white">
-                            <div className="flex flex-row items-center gap-2 mb-2">
-                                <Coffee size={25} className="text-teal-500" />
-                                <span className="font-semibold">
-                                    Private Cabanas
-                                </span>
-                            </div>
-                            <p className="text-left text-[.7rem] font-light mb-3">
-                                20 exclusive air-conditioned &lsquo;small houses&rsquo; with
-                                minibars and plush daybeds.
-                            </p>
-                        </div>
-                    </div>
+                    <PoolCard
+                        src="/thermal-pool.jpg"
+                        icon={<Sun size={25} className="text-teal-500" />}
+                        title="Thermal Pool"
+                        description="A circular hot pool built on the terrace edge for therapeutic soaking with a view."
+                    />
+                    {/* Private Cabanas */}
+                    <PoolCard
+                        src="/air-cabana.jpg"
+                        icon={<Coffee size={25} className="text-teal-500" />}
+                        title="Private Cabanas"
+                        description="20 exclusive air-conditioned &lsquo;small houses&rsquo; with minibars and plush daybeds."
+                    />
                 </div>
             </div>
         </section>
